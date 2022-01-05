22bd9731783de18c32573dddc825ec2263b8a585
Mariavellous <mm.alcaide38@gmail.com>
2022-01-04 21:09:29 -0800

    Finished challenge 4

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for
 
+
 app = Flask(__name__)
+app.secret_key = "elephant"
 
 all_books = []
 
-
 @app.route('/')
 def home():
-    pass
+    return render_template('index.html', data=all_books)
 
-
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    if request.method == "POST":
+        new_book = {
+            "title": request.form["title"],
+            "author": request.form["author"],
+            "rating": request.form["rating"]
+        }
+        all_books.append(new_book)
+        print(all_books)
+
+        # NOTE: You can use the redirect method from flask to redirect to another route
+        # e.g. in this case to the home page after the form has been submitted.
+        return redirect(url_for('home'))
+
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
